0dbd70d7eab08ed8cf4a23fd04f2e308f3ce9f07
Nikolas Vahlas <nikolasvahlas@nikolasvahlas.com>
2023-08-22 17:22:54 -0700

    added functionality of adding a movie

--- script.js
+++ script.js
@@ -2,10 +2,31 @@ const deleteButtons = document.getElementsByClassName('x-button');
 
 for (let x of deleteButtons) {
   x.addEventListener('click', deleteListElement);
-  console.log('Event listener added to button');
 }
 
+const addButton = document.getElementById('add-button');
+
+addButton.addEventListener('click', addMovie);
+
 function deleteListElement() {
-  console.log('red');
   this.parentElement.parentElement.remove();
+}
+
+function addMovie() {
+  const moviespace = document.getElementById('add-movie-field');
+  const textValue = moviespace.value;
+  if (textValue === '') {
+    alert('Please input a name');
+  } else {
+    const elementList = document.getElementsByClassName('movie--list__entry');
+    const elementToClone = elementList[0];
+    const clonedLi = elementToClone.cloneNode(true);
+    const child = clonedLi.children[0];
+    const grandchild = child.children[0];
+    grandchild.innerHTML = `${textValue}`;
+    const movieList = document.getElementById('movie--list');
+    const clonedDeleteButton = clonedLi.getElementsByClassName('x-button')[0];
+    clonedDeleteButton.addEventListener('click', deleteListElement);
+    movieList.appendChild(clonedLi);
+  }
 }
